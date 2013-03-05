7bb2f2ba7d255753ea0b06786487a46496d2ee02
David Worley <dave@instructables.com>
2013-03-04 21:49:02 -0800

    Added the non-working BLE code

--- SuperEagle/ZENSuperEagleDashboard.h
+++ SuperEagle/ZENSuperEagleDashboard.h
@@ -7,9 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BLE.h"
 
-@interface ZENSuperEagleDashboard : UIViewController
+@interface ZENSuperEagleDashboard : UIViewController <BLEDelegate>
 
+@property (strong, nonatomic) BLE *bleShield;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
 @property (weak, nonatomic) IBOutlet UISlider *leftMotorSlider;
 @property (weak, nonatomic) IBOutlet UISlider *motorSlider;
 @property (weak, nonatomic) IBOutlet UISlider *rightMotorSlider;
@@ -17,12 +20,11 @@
 - (IBAction) leftMotorSliderChanged:(id)sender;
 - (IBAction) motorSliderChanged:(id)sender;
 - (IBAction) rightMotorSliderChanged:(id)sender;
-
 - (IBAction) motorSliderWasReleased:(id)sender;
-
 - (NSArray *) realignMotorSliders:(NSArray *)motorSliders;
 - (NSArray *) positionMotorSliders:(NSArray *)motorSliders;
-
 - (NSString *) stringForSlider:(UISlider *)slider;
+- (void) connectionTimer:(NSTimer *)timer;
+- (IBAction)scanForPeripherals:(id)sender;
 
 @end
--- SuperEagle/ZENSuperEagleDashboard.m
+++ SuperEagle/ZENSuperEagleDashboard.m
@@ -26,7 +26,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
+    self.bleShield = [[BLE alloc] init];
+    [self.bleShield controlSetup:1];
+    self.bleShield.delegate = self;
+
     NSArray *sliders = [NSArray arrayWithObjects:self.leftMotorSlider,
                                                  self.motorSlider,
                                                  self.rightMotorSlider, nil];
@@ -79,22 +83,123 @@
 #pragma mark - Event Handling
 - (IBAction) leftMotorSliderChanged:(id)sender
 {
+    UISlider *slider = (UISlider *)sender;
+    NSInteger index = roundl(slider.value);
+    NSData *payload = [NSData dataWithBytes:&index length:sizeof(index)];
+    [self.bleShield write:payload];
 }
 - (IBAction) motorSliderChanged:(id)sender
 {
+    UISlider *slider = (UISlider *)sender;
+    NSInteger index = roundl(slider.value);
+    NSData *payload = [NSData dataWithBytes:&index length:sizeof(index)];
+    [self.bleShield write:payload];
 }
 - (IBAction) rightMotorSliderChanged:(id)sender
 {
+    UISlider *slider = (UISlider *)sender;
+    NSInteger index = roundl(slider.value);
+    NSData *payload = [NSData dataWithBytes:&index length:sizeof(index)];
+    [self.bleShield write:payload];
 }
 - (IBAction) motorSliderWasReleased:(id)sender
 {
     [UIView animateWithDuration:0.25f animations:^{
         [(UISlider *)sender setValue:0];
     }];
+    NSUInteger index = 0;
+    NSData *payload = [NSData dataWithBytes:&index length:sizeof(index)];
+    [self.bleShield write:payload];
 }
 - (NSString *) stringForSlider:(UISlider *)slider
 {
     return [NSString stringWithFormat:@"%f", slider.value];
 }
+
+#pragma mark - BLE stuff
+// Called when scan period is over to connect to the first found peripheral
+-(void) connectionTimer:(NSTimer *)timer
+{
+    if (self.bleShield.peripherals.count > 0) {
+        [self.bleShield connectPeripheral:[self.bleShield.peripherals objectAtIndex:0]];
+    }
+}
+
+-(void) bleDidReceiveData:(unsigned char *)data length:(int)length
+{
+    NSData *d = [NSData dataWithBytes:data length:length];
+    NSString *s = [[NSString alloc] initWithData:d encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", s);
+}
+
+- (void) bleDidDisconnect
+{
+    //[self.buttonConnect setTitle:@"Connect" forState:UIControlStateNormal];
+}
+
+-(void) bleDidConnect
+{
+    [self.spinner stopAnimating];
+    //[self.buttonConnect setTitle:@"Disconnect" forState:UIControlStateNormal];
+}
+
+-(void) bleDidUpdateRSSI:(NSNumber *)rssi
+{
+    NSLog(@"%@", rssi.stringValue);
+}
+
+- (IBAction)scanForPeripherals:(id)sender
+{
+    if (self.bleShield.activePeripheral) {
+        if(self.bleShield.activePeripheral.isConnected)
+        {
+            [[self.bleShield CM] cancelPeripheralConnection:[self.bleShield activePeripheral]];
+            return;
+        }
+    }
+
+    if (self.bleShield.peripherals) {
+        self.bleShield.peripherals = nil;
+    }
+
+    [self.bleShield findBLEPeripherals:3];
+
+    [NSTimer scheduledTimerWithTimeInterval:(float)7.0 target:self selector:@selector(connectionTimer:) userInfo:nil repeats:NO];
+}
+
+//- (IBAction)BLEShieldSend:(id)sender
+//{
+//    NSString *s;
+//    NSData *d;
+//
+//    if (self.textField.text.length > 16)
+//        s = [self.textField.text substringToIndex:16];
+//    else
+//        s = self.textField.text;
+//
+//    s = [NSString stringWithFormat:@"%@\r\n", s];
+//    d = [s dataUsingEncoding:NSUTF8StringEncoding];
+//
+//    [bleShield write:d];
+//}
+//
+//- (IBAction)BLEShieldScan:(id)sender
+//{
+//    if (bleShield.activePeripheral)
+//        if(bleShield.activePeripheral.isConnected)
+//        {
+//            [[bleShield CM] cancelPeripheralConnection:[bleShield activePeripheral]];
+//            return;
+//        }
+//
+//    if (bleShield.peripherals)
+//        bleShield.peripherals = nil;
+//
+//    [bleShield findBLEPeripherals:3];
+//
+//    [NSTimer scheduledTimerWithTimeInterval:(float)3.0 target:self selector:@selector(connectionTimer:) userInfo:nil repeats:NO];
+//
+//    [self.spinner startAnimating];
+//}
 
 @end
